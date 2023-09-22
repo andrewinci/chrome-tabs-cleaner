0c11aa467da96cbf46f051708069b0210f7a6455
andrewinci <andrewinci@gmail.com>
2023-09-22 07:15:08 +0100

    feat: move all tabs to the current window

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,27 @@
 // background.js
-chrome.action.onClicked.addListener(() => {
-  console.log("Clicked");
-});
 
+const onRun = async () => {
+  console.log("Start moving windows");
+  const currentWin = await chrome.windows.getCurrent();
+  const allWindows = await chrome.windows.getAll();
+  const allTabs = (
+    await Promise.all(
+      allWindows
+        .map((w) => chrome.tabs.query({ windowId: w.id }))
+    )
+  ).flatMap((t) => t);
+
+  allTabs
+    .sort((a, b) => b.title?.localeCompare(a.title ?? "") ?? 0)
+    .forEach((t) => {
+      if (t?.id) {
+        chrome.tabs.move(t.id, {
+          windowId: currentWin.id,
+          index: 0,
+        });
+      }
+    });
+};
+
+chrome.action.onClicked.addListener(() => onRun());
 export {};
